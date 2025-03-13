a1895b7fbec4b393f0ac4cf4f2c88c92a24af7d8
Shakirali62050 <164683390+Shakirali62050@users.noreply.github.com>
2025-03-13 17:02:28 +0530

    Add files via upload

---
+++ concept.py
@@ -0,0 +1,24 @@
+# this is single line comment
+
+""""
+tis is multiple line
+comment
+"""
+# variable and constant
+a = 6
+a = 5
+name = " shakir ali"
+name = "mohmmad"
+
+#type of data
+a=5 # <class 'int'>
+b=2.3# <class 'floatt'>
+c= "shakir"# <class 'string'>
+d = True# <class 'bool'>
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#Type casting
+print(int(b))
